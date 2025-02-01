5a53fb552e50425c20b5a12846b85798f7757f7a
rdemarteau <raymond.demarteau93@gmail.com>
2025-02-01 22:21:17 +0100

    Added some extra routing to installers.

--- x86_64-apple-darwin/release/kismet-core-installer.d
+++
@@ -1,1 +0,0 @@
-/Users/raymond/Documents/Kismet-FINAL/kismet-core-installer/target/x86_64-apple-darwin/release/kismet-core-installer: /Users/raymond/Documents/Kismet-FINAL/kismet-core-installer/src/main.rs
