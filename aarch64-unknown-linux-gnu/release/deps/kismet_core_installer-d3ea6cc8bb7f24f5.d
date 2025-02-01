b1a67e0727935a4c62b96171c70471ba319158ff
rdemarteau <raymond.demarteau93@gmail.com>
2025-02-01 21:12:04 +0100

    Added installer for Linux ARM architecture

---
+++ aarch64-unknown-linux-gnu/release/deps/kismet_core_installer-d3ea6cc8bb7f24f5.d
@@ -0,0 +1,5 @@
+/root/kismet-core-installer/target/aarch64-unknown-linux-gnu/release/deps/kismet_core_installer-d3ea6cc8bb7f24f5: src/main.rs
+
+/root/kismet-core-installer/target/aarch64-unknown-linux-gnu/release/deps/kismet_core_installer-d3ea6cc8bb7f24f5.d: src/main.rs
+
+src/main.rs:
---
+++ aarch64-unknown-linux-gnu/release/kismet-core-installer.d
@@ -0,0 +1,1 @@
+/root/kismet-core-installer/target/aarch64-unknown-linux-gnu/release/kismet-core-installer: /root/kismet-core-installer/src/main.rs
